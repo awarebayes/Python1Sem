9f64a8f5714076957db429f51e01718b20f5f61b
Mike S <awarebayes@gmail.com>
2020-10-06 13:52:34 +0300

    lab 4 finished

--- year_1/october/lab4_func_table.py
+++ year_1/october/lab4_func_table.py
@@ -1,12 +1,14 @@
 # Посторить таблицы функции
 # _________________________
 # |  x  |    f1   | f2    |
-# Определить min t1, t2
+# Определить min f1, f2
 # Постороить график одной из функций,
 # на оси необходимо поставить от 4 до 8 засечек пользователя
 #  Написал: Щербина МА ИУ7  15Б
 
 from math import inf, ceil
+from os import get_terminal_size
+from math import log
 
 N_FUN = 2  # number of functions
 COL_W = 15  # number of symbols per column
@@ -18,6 +20,7 @@ if start > end:
     print(f"Start should be less than end, got start ({start}) > end ({end})")
     exit(1)
 step_size = float(input(">>> range step (float): "))
+max_exp = int(log(max(map(abs, [start, end])), 10))
 
 n_steps = ceil((end - start) / step_size) + 1
 
@@ -27,10 +30,10 @@ max_f1 = -inf
 max_f2 = -inf
 
 print("-" * (3 * COL_W + 4))
-
 # 15 should be COL_W
 # cant do dynamic formatting without double format...
 print("|{:^15}|{:^15}|{:^15}|".format("x", "f1", "f2"))
+print("-" * (3 * COL_W + 4))
 for step in range(n_steps):
     x = start + step * step_size
     f1 = x - 0.5 ** x
@@ -50,13 +53,14 @@ print()
 
 # Plotting Part!
 # P.S. This is the actual plot size, not including axes and labels
-PLOT_W = 100  # plot width
+PLOT_W, _ = get_terminal_size()  # plot width
+PLOT_W -= 15 # adjust to fit better
 
 n_func = int(
     input("which function do you want to plot [1,2]? \n >>> ")
 )  # which function to plot
 y_ticks = int(
-    input("how many ticks do you want on the y axis [4, 8]? \n >>> ")
+    input("how many ticks do you want on the y axis [4..8]? \n >>> ")
 )  # number of ticks on y axis
 y_ticks -= 1
 y_should_tick1 = 0  # when number should be placed
@@ -96,7 +100,7 @@ for i in range(PLOT_W + 1):  # +1 is to print the last nu,ber / tick
         print("|", end="")  # tick
     else:
         print("-", end="")  # axis
-print("  -> y")
+print("--->y")
 
 
 # x axis
@@ -108,10 +112,11 @@ for step in range(n_steps):
         y = x ** 3 - 4.49 * x ** 2 - 24.5 * x + 19.5
     percent_y = (y - f_min) / f_range  # where y is in range, normalized between [0, 1]
     pos_y = int(percent_y * PLOT_W)  # where y is in [0, PLOT_W]
+    x = x / 10 ** max_exp
     if pos_y == 0:  # y is on the x axis
         print("{:>6.2f}".format(x), "*", sep="")
     else:
         print("{:>6.2f}".format(x), "|", " " * (pos_y - 1), "*", sep="")
 print(" " * 6 + "|")
 print(" " * 6 + "v")
-print(" " * 6 + "x")
+print(" " * 6 + "x*10^", max_exp, sep="")
