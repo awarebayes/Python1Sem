12e4126dc7e8785796b4169054f048960e04ce39
mike watts <awarebayes@gmail.com>
2020-10-19 22:49:22 +0300

    good night to all my homies

--- year_1/practice/func_MIDI.py
+++ year_1/practice/func_MIDI.py
@@ -1,7 +1,8 @@
 from collections import namedtuple
+import heapq
 
-ExprArg = namedtuple("ExprArg", ["name", "base_name", "state", "time_func"])
-Arg = namedtuple("Arg", ["time", "state", "chord"])
+ExprArg = namedtuple("ExprArg", ["base_name", "state", "time_offset"])
+UnnamedNote = namedtuple("UnnamedNote", ["time", "state"])
 
 # time function is a lambda which maps time relatively to coeficient
 programs = [
@@ -21,13 +22,7 @@ programs = [
     ["0 ON 60", "1 OFF 60", "1 ON 60", "10 OFF 60"],
 ]
 
-# ignore structs
-# time1 OFF, time2 OFF -> time2 OFF
-# time1 ON, time1 ON-> time1 ON
-# ~ time1 ON, time1+1 ON -> time1-1 ON, time1 OFF, time1+1 ON
-# time1 ON, time2 ON -> time1 ON, time2-1 OFF, time2 ON  | time1 != time2+1
-
-rules = [
+rules_str = [
     # 1 time, on
     "a ON, a ON -> a ON",
     "a-1 ON, a ON -> ? ?",  # i don't know what should happen there
@@ -45,58 +40,79 @@ rules = [
     # "a OFF, b OFF -> "
 ]
 
+
+notes = {}
+
 # get time functions and states for named args:
 # time1: { time_function: \x -> x-1, True }, ...
 def parse_expr(expr_args):
     target_args = []
     base_times = {}  # where names like a appear
     for idx, arg in enumerate(expr_args):
-
         name, state = arg.strip().split(" ")
         state = True if state == "ON" else False
         if "+" in name:
             base_name, const_add = name.split("+")  # name: a+1, base_name: a
-            const_add = int(const_add)
-            time_func = lambda t: t + const_add
-            base_name = name.split("+")[0]
+            time_offset = int(const_add)
         elif "-" in name:
             base_name, const_sub = name.split("-")
-            const_sub = int(const_sub)
-            time_func = lambda t: t - const_sub
+            time_offset = -int(const_sub)
         else:
             base_name = name
             base_times[name] = idx
-            time_func = lambda t: t
-        target_args.append(ExprArg(name, base_name, state, time_func))
+            time_offset = 0
+        target_args.append(ExprArg(base_name, state, time_offset))
     return target_args, base_times
 
-
+# parse a function (rule), get functions for matching and evaluation 
 def parse_func(expr):
     expr_args, expr_res = expr.split("->")
     expr_args, expr_res = expr_args.split(","), expr_res.split(",")
     expr_args, arg_times = parse_expr(expr_args)
-    expr_res, res_times = parse_expr(expr_res)
+    expr_res, _ = parse_expr(expr_res)
     args_len = len(expr_args)
 
     # matched args and parsed: time1: 10, time2: 11, ...
     def match(args):
-        if len(args) < args_len:
+        if len(args) != args_len:
             return False
         for arg, expr_arg in zip(args, expr_args):
             time, state = arg.time, arg.state
             base_arg_time = args[arg_times[expr_arg.base_name]].time
-            if expr_arg.time_func(base_arg_time) != time or state != expr_arg.state:
+            if base_arg_time + expr_arg.time_offset != time or state != expr_arg.state:
                 return False
         return True
 
     def results(args):
-        pass
+        out = []
+        for res in expr_res:
+            base_res_time = args[arg_times[res.base_name]].time
+            res_time = base_res_time + res.time_offset
+            out.append(UnnamedNote(res_time, res.state))
+        return out
 
-    return match, None
+    return match, results
+
+
+# set [match_f, results_f] for each rule in rules_str
+rules = list(map(parse_expr(rules_str)))
+
+# do guard pattern matching, like in haskell
+# if matched then evaluate and return
+# otherwise False
+def match_evaluate(args):
+    for match_f, results_f in rules:
+        if match_f(args):
+            return results_f(args)
+    return False
+
 
 
 def main():
+    match_f, results_f = parse_func(rules[0])
     print(rules[0])
+
+
 
 
 if __name__ == "__main__":
--- year_1/practice/tests.py
+++ year_1/practice/tests.py
@@ -1,4 +1,10 @@
-from func_MIDI import parse_func, Arg
+from func_MIDI import parse_func, UnnamedNote
+
+#                  _       _
+#  _ __ ___   __ _| |_ ___| |__
+# | '_ ` _ \ / _` | __/ __| '_ \
+# | | | | | | (_| | || (__| | | |
+# |_| |_| |_|\__,_|\__\___|_| |_|
 
 match_tests = [
     ("a ON, a ON -> a ON", [(10, True), (10, True)], True),
@@ -17,7 +23,7 @@ match_tests = [
 print("Running match tests...")
 for func, args, target_res in match_tests:
     match_f, _ = parse_func(func)
-    args_turple = [Arg(arg[0], arg[1], 0) for arg in args]
+    args_turple = [UnnamedNote(arg[0], arg[1]) for arg in args]
     match_res = match_f(args_turple)
     if match_res != target_res:
         print("Error while running test!")
@@ -26,4 +32,40 @@ for func, args, target_res in match_tests:
         print("target result:", target_res)
         exit()
 
-print("All match tests successfully passed!")
+print("All match tests successfully passed!\n")
+
+#  _ __ ___  ___ _   _| | |_ ___
+# | '__/ _ \/ __| | | | | __/ __|
+# | | |  __/\__ \ |_| | | |_\__ \
+# |_|  \___||___/\__,_|_|\__|___/
+
+match_tests = [
+    ("a ON, a ON -> a ON", [(10, True), (10, True)], [(10, True)]),
+    ("a OFF, a OFF -> a OFF", [(10, False), (10, False)], [(10, False)]),
+    ("a OFF, b OFF -> b OFF", [(10, False), (12, False)], [(12, False)]),
+    (
+        "a OFF, a ON -> a-1 OFF, a ON",
+        [(10, False), (10, True)],
+        [(9, False), (10, True)],
+    ),
+    (
+        "a ON, a OFF -> a-1 OFF, a ON",
+        [(10, True), (10, False)],
+        [(9, False), (10, True)],
+    ),
+]
+print("Running results tests...")
+for func, args, target_res in match_tests:
+    _, results_f = parse_func(func)
+    args_turple = [UnnamedNote(arg[0], arg[1]) for arg in args]
+    res_turple = [UnnamedNote(res[0], res[1]) for res in target_res]
+    results = results_f(args_turple)
+    if results != res_turple:
+        print("Error while running test!")
+        print("args:", args)
+        print("result:", results)
+        print("target result:", res_turple)
+        exit()
+
+print("All retults tests successfully passed!\n")
+print("All tests successfully passed!")
